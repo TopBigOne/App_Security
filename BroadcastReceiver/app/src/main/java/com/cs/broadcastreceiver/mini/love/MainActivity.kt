a6fd981b1f6f35475d910cee9bf3032cf97dd543
guangya.zhou <chinagyzhou@gmail.com>
2022-08-26 15:02:30 +0800

    研究 广播漏洞

---
+++ BroadcastReceiver/app/src/main/java/com/cs/broadcastreceiver/mini/love/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.cs.broadcastreceiver.mini.love
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import javax.net.ssl.SSLSocket
+import javax.net.ssl.X509TrustManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
