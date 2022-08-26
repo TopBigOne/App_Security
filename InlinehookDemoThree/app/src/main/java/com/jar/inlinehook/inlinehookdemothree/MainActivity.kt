70cafa0b17372ba7479cb2c0d0f8b5d72e76fad2
guangya.zhou <chinagyzhou@gmail.com>
2022-08-26 15:03:40 +0800

    依旧是inline hook

---
+++ InlinehookDemoThree/app/src/main/java/com/jar/inlinehook/inlinehookdemothree/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.jar.inlinehook.inlinehookdemothree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.jar.inlinehook.inlinehookdemothree.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initEvent()
+    }
+
+
+    private fun initEvent() {
+        binding.testInlineHookOne.setOnClickListener {
+            val result = stringFromJNI()
+            Log.d(TAG, "initEvent----------：  $result")
+        }
+
+        binding.testInlineHookTwo.setOnClickListener {
+            testHookTwo()
+        }
+
+    }
+
+    /**
+     * A native method that is implemented by the 'inlinehookdemo1' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    external fun testHookTwo()
+
+
+    companion object {
+        private const val TAG = "MainActivity : "
+
+        // Used to load the 'inlinehookdemothree' library on application startup.
+        init {
+            System.loadLibrary("inline_hook_demo_three")
+        }
+    }
+}
---
+++ InlinehookDemoTwo/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,72 @@
+#include <jni.h>
+#include <string>
+#include <dlfcn.h>
+#include "include/inlineHook.h"
+
+
+#include <android/log.h>
+
+const char *TAG = "xiao_ya";
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__))
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__))
+
+
+void *(*oldstr)(char *, char *);
+
+void *newstr(char *a, char *b) {
+    LOGI("%s%s", a, b);
+    int c = 1;
+    return &c;
+}
+
+bool hook(char *str) {
+    LOGI("invoke hook()");
+    if (strstr(str, "windaa")) {
+        LOGI("hook success");
+        return true;
+    }
+    LOGE("hook FAILURE----讨厌讨厌讨厌");
+    return false;
+}
+
+void startHook() {
+    LOGI("invoke startHook()");
+    void *libc = dlopen("libc.so", RTLD_NOW);
+    if (libc == nullptr) {
+        LOGE("获取 libc 失败");
+        return;
+    }
+    LOGI("获取 libc 成功-666");
+    void *str_addr = dlsym(libc, "strstr");
+    if (registerInlineHook((uint32_t) str_addr, (uint32_t) newstr, (uint32_t **) oldstr) !=
+        ELE7EN_OK) {
+        LOGE("registerInlineHook failure");
+        return;
+    }
+
+    if (inlineHook((uint32_t) str_addr) == ELE7EN_OK) {
+        LOGI("registerInlineHook hook success");
+    }
+
+    LOGE("0000000000000000");
+
+
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_jar_inlinehook_demotwo_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+
+    hook("fddsfd");
+    return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_jar_inlinehook_demotwo_MainActivity_testHookTwo(JNIEnv *env, jobject thiz) {
+    startHook();
+}
